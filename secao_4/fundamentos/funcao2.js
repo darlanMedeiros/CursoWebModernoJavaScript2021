e31831b35f63421a0c18ab6cf3f812835d958f8d
darlanMedeiros <darlan.smedeiros@gmail.com>
2021-11-18 14:07:37 -0300

    funcao

---
+++ secao_4/fundamentos/funcao2.js
@@ -0,0 +1,18 @@
+//Armazenanado uma funcao em uma variavel
+
+const imprimirSoma = function(a, b){
+    console.log(a+ b)
+}
+
+imprimirSoma(2,6)
+
+//Armazenando um função arrow em uma variavel
+
+const soma = (a,b) => {
+
+}
+
+
+const subtracao = (a, b) => a-b
+
+console.log(subtracao(2, 3))
---
+++ secao_4/fundamentos/nullUndefined.js
@@ -0,0 +1,17 @@
+let valor
+// Valor não foi inicializado
+console.log(valor)
+// Não aponta para nenhum lugar da memoria.
+
+valor = null
+console.log(valor)
+
+const produto = {}
+
+console.log(produto.preco)
+
+console.log (produto)
+
+produto.preco =3.5
+
+console.log (produto)
