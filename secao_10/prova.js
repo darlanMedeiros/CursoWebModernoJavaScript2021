4048317c4423f72d5cca1d4955343330e39c7f7e
darlanMedeiros <darlan.smedeiros@gmail.com>
2021-11-24 21:37:50 -0300

    ESnext

---
+++ secao_10/prova.js
@@ -0,0 +1,654 @@
+/*
+
+
+A equipe de desenvolvedores da Magazine Luiza (Para você é Magalu!), uma das maiores redes varejistas do Brasil, está testando um algoritmo que realiza cálculos automáticos. O fluxograma abaixo mostra como o algoritmo funciona:
+
+
+
+Ao executar o algoritmo, qual é o valor impresso ao final?
+
+A.
+
+5 23
+
+B.
+
+4 15
+
+C.
+
+6 33
+
+D.
+
+6 23
+
+E.
+
+1 3
+
+02
+
+“Não é verdade que todos os parceiros daquele marketplace não atualizam seus produtos”. `
+
+A condição para que a afirmação acima seja verdadeira é que seja verdadeira:
+
+A.
+
+No máximo um parceiro daquele marketplace não atualiza seus produtos.
+
+B.
+
+Todos os parceiros daquele marketplace atualizam seus produtos.
+
+C.
+
+Pelo menos um parceiro daquele marketplace atualiza seus produtos.
+
+D.
+
+Nenhum parceiro daquele marketplace não atualiza seus produtos.
+
+E.
+
+Nenhum parceiro daquele marketplace atualiza seus produtos.
+
+03
+
+Considere a proposição:
+
+“Débora é programadora ou Débora é cientista de dados e matemática”
+
+Débora não é programadora. Logo, conclui-se que:
+
+A.
+
+“Débora não é cientista de dados e não é matemática.”
+
+B.
+
+“Débora não é cientista de dados ou não é matemática.”
+
+C.
+
+“Débora é cientista de dados e matemática.”
+
+D.
+
+“Débora é cientista de dados ou matemática.”
+
+E.
+
+“Débora é cientista de dados e não é matemática.”
+
+04
+
+Em um galpão de uma rede de E-commerce (comércio eletrônico) há 5 empilhadeiras alocadas em 5 docas, sendo: Empilhadeira P, Empilhadeira Q, Empilhadeira R, Empilhadeira S e Empilhadeira T. Estas ficam lado a lado. Não necessariamente nessa ordem. Certo dia, as empilhadeiras ficam dispostas da seguinte forma:
+
+I. Há exatamente uma doca entre a Empilhadeira Q e Empilhadeira R; II. Existem 2 ou 3 docas entre Empilhadeira P e Empilhadeira T; III. Nem a Empilhadeira T e nem Empilhadeira R estão ao lado da Empilhadeira S.
+
+Dadas as informações acima, qual das cinco empilhadeiras está na doca do meio?
+
+A.
+
+Empilhadeira P
+
+B.
+
+Empilhadeira Q
+
+C.
+
+Empilhadeira R
+
+D.
+
+Empilhadeira S
+
+E.
+
+Empilhadeira T
+
+05
+
+Um site de varejo eletrônico faz o registro de seus produtos por códigos, representados por conjuntos de caracteres. A sequência abaixo simula os registros de algumas compras feitas no site:
+
+Compra 1 | 66IO P45T 678U LKJ7: CAFETEIRA NOTEBOOK SECADOR FOGÃO Compra 2 | F2D3 P45T A23S JET1: RELÓGIO CELULAR PANELA FOGÃO Compra 3 | 888R PK49 777Y JET1: TABLET CELULAR FRITADEIRA SOFÁ Compra 4 | Y7GH PK49 A23S LKJ7: RELÓGIO NOTEBOOK BERÇO SOFÁ
+
+Se o sistema recebe o conjunto de caracteres 888R P45T A23S LKJ7, qual das seguintes sequências de produtos deve ser registrada?
+
+A.
+
+RELÓGIO NOTEBOOK FOGÃO BERÇO
+
+B.
+
+CAFETEIRA SECADOR NOTEBOOK SOFÁ
+
+C.
+
+PANELA CELULAR NOTEBOOK RELÓGIO
+
+D.
+
+CAFETEIRA BERÇO SOFÁ FOGÃO
+
+E.
+
+RELÓGIO NOTEBOOK FRITADEIRA FOGÃO
+
+06
+
+A caixa registradora de uma loja é composta por uma tela de exibição, um teclado com cinco teclas, sendo: J, K, L, M e N, e uma memória interna que armazena apenas um número inteiro e possui valor inicial zero, chamada de INT. As teclas podem ser pressionadas em qualquer sequência, gerando um resultado a partir dessa digitação. Cada tecla possui uma ação:
+
+J: INT é inicializada com o valor dez, independente do valor anterior. K: armazena o valor que INT possuía subtraído um. L: armazena o valor que INT possuía subtraído dois. M: armazena o valor que INT possuía somado um. N: imprime o valor contido em INT na tela.
+
+Qual sequência numérica deve ser exibida na tela a partir da digitação K M M J N N M N J K N, respectivamente?
+
+A.
+
+10, 11, 10, 11
+
+B.
+
+10, 10, 11, 9
+
+C.
+
+9, 11, 10, 9
+
+D.
+
+10, 10, 10, 11
+
+E.
+
+11, 9, 9, 10
+
+07
+
+Qual o vigésimo elemento da seguinte sequência lógica?
+
+19, 2, 27, 5, 35, 8, ...
+
+A.
+
+29
+
+B.
+
+26
+
+C.
+
+91
+
+D.
+
+14
+
+E.
+
+99
+
+08
+
+Num supermercado há 3 caixas em funcionamento, sendo: X, Y e Z. O caixa X atende 2 clientes por minuto; o caixa Y atende 1,5 clientes por minuto; e o caixa Z atende 0,5 clientes por minuto. A princípio, os caixas não estão atendendo ninguém e a fila está com 100 clientes.
+
+O tempo que cada um dos caixas X, Y e Z leva para atender um único cliente é respectivamente de:
+
+A.
+
+90 seg., 100 seg. e 120 seg.
+
+B.
+
+60 seg., 120 seg. e 160 seg.
+
+C.
+
+1 min., 2 min. e 3 min.
+
+D.
+
+30 seg., 40 seg. e 2 min.
+
+E.
+
+0,5 min., 30 seg. e 90 seg.
+
+09
+
+Na seção "Tem no Magalu", a categoria "Livros" passa por uma atualização de catálogo no gênero "Informática e Tecnologia". Ao todo, são incluídos 10 livros, que podem ser divididos em três assuntos diferentes: (i) Banco de Dados, (ii) Segurança e (iii) Linguagens de Programação, conforme a tabela.
+
+Banco de Dados	Segurança	Linguagens de Programação
+Use a cabeça! SQL	Políticas da imagem	Java: Ensino didático
+Modelagem e Banco de Dados	Cibersegurança para leigos	Data Science do Zero
+Estatística Prática para Cientistas de Dados	Segurança e privacidade na rede	Python e Django
+-	-	Código Limpo
+Sabendo disso, de quantas maneiras é possível ordenar os livros, de forma que eles fiquem juntos pelo mesmo assunto?
+
+A.
+
+864
+
+B.
+
+5184
+
+C.
+
+2592
+
+D.
+
+216
+
+E.
+
+8640
+
+10
+
+Em uma fábrica de perfumes, João é responsável por dividir um lote da produção em caixas de maneira que cada caixa contenha o mesmo número de frascos de um determinado perfume. João sabe que ele pode colocar 12 frascos em cada caixa, mas também é possível colocar 15 em cada caixa, ou até mesmo 18. Em qualquer uma dessas opções, todos os frascos do lote ficarão guardados, não sobrando frasco algum fora das caixas. Sabendo disso, qual é o menor número de frascos de perfume desse lote?
+
+A.
+
+360
+
+B.
+
+216
+
+C.
+
+180
+
+D.
+
+270
+
+E.
+
+120
+
+11
+
+Na Black das Blacks, Black Friday da Magazine Luiza, são anunciadas as seguintes promoções:
+
+I. Compre um Notebook e ganhe 20% de desconto na compra de uma Impressora. Nessa promoção, a soma do valor dos dois produtos sai por R$3120,00, já aplicado o desconto.
+
+II. Compre um Notebook e ganhe 10% de desconto na compra de um Projetor. Nessa promoção, a soma do valor dos dois produtos sai por R$3360,00, já aplicado o desconto.
+
+III. Compre uma Impressora e ganhe 30% de desconto no Projetor. Nessa promoção, a soma do valor dos dois produtos sai por R$4080,00, já aplicado o desconto.
+
+Sabendo disso, qual é o valor integral da Impressora?
+
+A.
+
+R$ 1200,00
+
+B.
+
+R$ 1800,00
+
+C.
+
+R$ 2000,00
+
+D.
+
+R$ 2400,00
+
+E.
+
+R$ 2800,00
+
+12
+
+Sabe-se que 60% do total de alunos de uma faculdade estão no curso de engenharia. Desses 60%, tem-se que 70% cursam engenharia mecânica, 20% cursam engenharia elétrica e 10% cursam engenharia química. Sabendo que 96 alunos cursam engenharia elétrica, qual é o total de alunos da faculdade?
+
+A.
+
+800
+
+B.
+
+480
+
+C.
+
+1200
+
+D.
+
+200
+
+E.
+
+768
+
+13
+
+No departamento comercial de uma empresa trabalham 5 funcionários com média de idade de 35 anos. Um novo funcionário é contratado e, com isso, a média de idade dos funcionários do departamento passa a ser 36,5 anos. Sabendo disso, quantos anos tem o novo funcionário?
+
+A.
+
+41
+
+B.
+
+42
+
+C.
+
+43
+
+D.
+
+44
+
+E.
+
+45
+
+14
+
+A turma de alunos do curso Data Science Degree da Let's Code deseja nomear dois alunos como representante e vice-representante de sala, respectivamente. Esses cargos serão escolhidos através de dois sorteios consecutivos. Ao cargo de representante, concorrem um aluno e uma aluna. Já ao cargo de vice-representante, concorrem um aluno e duas alunas. Sabendo que todos os candidatos têm a mesma chance de serem sorteados, qual é a probabilidade de o representante e de o vice-representante da turma serem ambos alunas?
+
+A.
+
+1/6
+
+B.
+
+5/6
+
+C.
+
+2/3
+
+D.
+
+1/5
+
+E.
+
+1/3
+
+15
+
+O time de pedagogia da Let's Code realizou uma pesquisa com 100 professores para escolher em qual dia da semana seriam realizadas as reuniões de alinhamento da área: terça-feira ou quinta-feira. O formulário da pesquisa permitia que os professores marcassem apenas terça-feira, apenas quinta-feira, ambos os dias ou nenhum dos dias. A pesquisa gerou os seguintes resultados:
+
+50 professores marcaram "terça-feira"
+70 professores marcaram "quinta-feira"
+15 professores não marcaram nem "terça-feira" e nem "quinta-feira"
+Sabendo disso, responda: quantos professores marcaram apenas "quinta-feira"?
+
+A.
+
+20
+
+B.
+
+35
+
+C.
+
+70
+
+D.
+
+50
+
+E.
+
+55
+
+16
+
+Finalizadas as inscrições para o projeto "Let's Code + Magalu", a equipe de dados do processo seletivo levantou as seguintes informações:
+
+630 pessoas se inscreveram no processo;
+A razão entre o número de homens inscritos e o número de mulheres inscritas é 3/4.
+Sabendo disso, responda, quantas mulheres se inscreveram nesse processo?
+
+A.90
+
+B.120
+
+C.270
+
+D.320
+
+E.360
+
+17 Sabe-se que:
+
+ESCREVA "a", "b" é o comando que imprime a mensagem a b na tela;
+Variável é um espaço na memória do computador capaz de reter e apresentar um valor;
+CARACTER x cria a variável x que armazena dados do tipo texto;
+LEIA z é o comando que solicita uma informação ao usuário e a armazena na variável z;
++ é o operador matemático de adição que pode ser utilizado para concatenar textos.
+Observe o código abaixo:
+
+CARACTER nome, dia, mes
+
+ESCREVA "Bem-vindo(a) ao app Magalu"
+
+ESCREVA "Digite o seu nome: "
+LEIA nome
+
+ESCREVA "Digite o dia que você nasceu: "
+LEIA dia
+
+ESCREVA "Digite o mês que você nasceu: "
+LEIA mes
+
+ESCREVA "Login:", dia + nome + mes
+Qual será a última linha impressa pelo programa se o usuário digitar, nessa ordem, Luiza, 9 e 10?
+
+A.
+
+Luiza910
+
+B.
+
+Login: Luiza910
+
+C.
+
+Login:9Luiza10
+
+D.
+
+9Luiza10
+
+E.
+
+Login: 9Luiza10
+
+18
+
+Tem-se que:
+
+REAL x cria a variável x que armazena dados do tipo número real;
+<- atribui um valor a uma variável;
+SE divide o fluxo do programa em dois, um representado por ENTAO, que roda se a condição é satisfeita e o outro representado por SENAO, que roda quando a condição não é satisfeita;
+> é o operador relacional maior que;
+* é o operador matemático de multiplicação.
+ESCREVA "App Magalu - Resgate de Bônus para próxima compra"
+
+REAL valor, bonus
+
+ESCREVA "Digite o valor da sua compra, em reais: "
+LEIA valor
+
+SE valor > 1000 ENTAO FAÇA
+    bonus <- valor * 0.2
+SENAO FAÇA
+    SE valor > 500 ENTAO FAÇA
+        bonus <- valor * 0.1
+    SENÂO FAÇA
+        bonus <- valor * 0.05
+    FIM_SE
+FIM_SE
+
+ESCREVA bonus
+Silvio, Márcia e Isabel compraram produtos pelo app Magalu e utilizaram o programa acima para resgatar o bônus para a próxima compra. Sabendo que Silvio gastou 1200 reais, Márcia gastou 500 reais e Isabel gastou 750 reais, qual foi o valor do bônus impresso na última linha do programa para Silvio, Márcia e Isabel, respectivamente?
+
+A.
+
+240, 25 e 37.5
+
+B.
+
+360, 75 e 78.75
+
+C.
+
+240, 50 e 75
+
+D.
+
+240, 25 e 75
+
+E.
+
+120, 25 e 75
+
+19
+
+Sabe-se que:
+
+INTEIRO x cria a variável x que armazena dados do tipo número inteiro;
+ENQUANTO repete um trecho de código até que sua condição torne-se falsa;
+OU é o operador lógico que resulta em um valor verdadeiro se pelo menos um dos valores de entrada da operação for verdadeiro, caso contrário o resultado é falso;
++ é o operador matemático de adição;
+< e > são os operadores de comparação menor e maior, respectivamente.
+Analise o código abaixo:
+
+ESCREVA "App Magalu - Sistema de segurança"
+
+INTEIRO int, i
+
+ESCREVA "Para continuar, digite um número entre 0 e 125: "
+LEIA int
+
+i <- 0
+
+ENQUANTO (int < 0) OU (int > 125) FAÇA
+    ESCREVA "Valor inválido"
+    i <- i + 1
+    ESCREVA "Para continuar, digite um número entre 0 e 125: "
+    LEIA int
+    
+
+FIM_ENQUANTO
+
+ESCREVA i
+Se a última linha impressa pelo programa foi 5, quantas vezes foi solicitado que o usuário digitasse um número? Se necessário, utilize os comandos definidos na questão anterior.
+
+A.
+
+1
+
+B.
+
+3
+
+C.
+
+5
+
+D.
+
+6
+
+E.
+
+7
+
+20
+
+Observe o fluxograma abaixo:
+
+
+
+Utilizando os comandos definidos nas questões anteriores, responda: qual alternativa melhor representa o fluxograma convertido para código?
+
+A.
+
+INTEIRO a, b
+
+LEIA a
+LEIA b
+
+ENQUANTO a < 5 FAÇA
+    b <- 2*a + b
+    a <- a + 1
+FIM_ENQUANTO
+
+ESCREVA a, b
+B.
+
+INTEIRO a, b
+
+a <- 1
+b <- 3
+
+ENQUANTO a < 5 FAÇA
+    b <- 2*a + b
+FIM_ENQUANTO
+
+a <- a + 1
+
+ESCREVA a, b
+C.
+
+INTEIRO a, b
+
+a <- 1
+b <- 3
+
+ENQUANTO a < 5 FAÇA
+    b <- 2*a + b
+    a <- a + 1
+FIM_ENQUANTO
+
+ESCREVA a, b
+D.
+
+INTEIRO a, b
+
+a <- 1
+b <- 3
+
+SE a < 5 ENTAO FAÇA
+    b <- 2*a + b
+    a <- a + 1
+SENAO FAÇA
+    ESCREVA a, b
+FIM_SE
+E.
+
+INTEIRO a, b
+
+ESCREVA "Digite 1: "
+LEIA a
+
+ESCREVA "Digite 3: "
+LEIA b
+
+ENQUANTO a < 5 FAÇA
+    b <- 2*a + b
+    a <- a + 1
+FIM_ENQUANTO
+
+LEIA a, b
+
+Finalizar prova
+
+*/
+
+
